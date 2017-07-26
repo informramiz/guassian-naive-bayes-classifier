a7d86584e7f3aa36aa819abd591ffe6fadd4037e
Ramiz Raja <informramiz@gmail.com>
2017-07-26 08:07:19 +0500

    chore: Create an empty eclipse project

---
+++ src/GuassianNaiveBayesClassifier.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : GuassianNaiveBayesClassifier.cpp
+// Author      : Ramiz Raja
+// Version     :
+// Copyright   : MIT Licensed
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	return 0;
+}
