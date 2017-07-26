f375bec6bd08afb29b6473cc02c72cbba657f12c
Ramiz Raja <informramiz@gmail.com>
2017-07-26 09:09:46 +0500

    refactor: Extract test code into a method

--- src/main.cpp
+++ src/main.cpp
@@ -59,39 +59,34 @@ vector<string> Load_Label(string file_name)
 
     }
 
-int main() {
-
-  vector< vector<double> > X_train = Load_State("./data/train_states.txt");
-  vector< vector<double> > X_test  = Load_State("./data/test_states.txt");
-  vector< string > Y_train  = Load_Label("./data/train_labels.txt");
-  vector< string > Y_test   = Load_Label("./data/test_labels.txt");
-
+void UdacityTestCode() {
+  vector<vector<double> > X_train = Load_State("./data/train_states.txt");
+  vector<vector<double> > X_test = Load_State("./data/test_states.txt");
+  vector<string> Y_train = Load_Label("./data/train_labels.txt");
+  vector<string> Y_test = Load_Label("./data/test_labels.txt");
   cout << "X_train number of elements " << X_train.size() << endl;
   cout << "X_train element size " << X_train[0].size() << endl;
   cout << "Y_train number of elements " << Y_train.size() << endl;
-
   GNB gnb = GNB();
-
   gnb.train(X_train, Y_train);
-
   cout << "X_test number of elements " << X_test.size() << endl;
   cout << "X_test element size " << X_test[0].size() << endl;
   cout << "Y_test number of elements " << Y_test.size() << endl;
-
   int score = 0;
-  for(int i = 0; i < X_test.size(); i++)
-  {
+  for (int i = 0; i < X_test.size(); i++) {
     vector<double> coords = X_test[i];
     string predicted = gnb.predict(coords);
-    if(predicted.compare(Y_test[i]) == 0)
-    {
+    if (predicted.compare(Y_test[i]) == 0) {
       score += 1;
     }
   }
-
   float fraction_correct = float(score) / Y_test.size();
-  cout << "You got " << (100*fraction_correct) << " correct" << endl;
+  cout << "You got " << (100 * fraction_correct) << " correct" << endl;
+}
 
+int main() {
+
+  UdacityTestCode();
   return 0;
 }
 
