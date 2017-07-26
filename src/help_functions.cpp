2445b1df4038845eee5baa33e418e2a6bcd4ef09
Ramiz Raja <informramiz@gmail.com>
2017-07-26 08:07:58 +0500

    feat: Add helper functions file copied from a previous project

---
+++ src/help_functions.cpp
@@ -0,0 +1,61 @@
+/*
+ * help_functions.cpp
+ *
+ *  Created on: Apr 24, 2017
+ *      Author: ramiz
+ */
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include "help_functions.h"
+
+HelpFunctions::HelpFunctions()
+  : ONE_OVER_SQRT_2PI(1/sqrt(2*M_PI)) {
+
+}
+
+//definition square:
+float HelpFunctions::squared(float x) {
+  return x*x;
+}
+
+/*****************************************************************************
+ * normpdf(X,mu,sigma) computes the probability function at values x using the
+ * normal distribution with mean mu and standard deviation std. x, mue and
+ * sigma must be scalar! The parameter std must be positive.
+ * The normal pdf is y=f(x;mu,std)= 1/(std*sqrt(2pi)) e[ -(x−mu)^2 / 2*std^2 ]
+ *****************************************************************************/
+float HelpFunctions::Normpdf(float x, float mu, float std) {
+  return (ONE_OVER_SQRT_2PI/std)*exp(-0.5*squared((x-mu)/std));
+}
+
+//function to normalize a vector:
+std::vector<float> HelpFunctions::NormalizeVector(std::vector<float> inputVector){
+
+  //declare sum:
+  float sum = 0.0f;
+
+  //declare and resize output vector:
+  std::vector<float> outputVector ;
+  outputVector.resize(inputVector.size());
+
+  //estimate the sum:
+  for (unsigned int i = 0; i < inputVector.size(); ++i) {
+    sum += inputVector[i];
+  }
+
+  //normalize with sum:
+  for (unsigned int i = 0; i < inputVector.size(); ++i) {
+    outputVector[i] = inputVector[i]/sum ;
+  }
+
+  //return normalized vector:
+  return outputVector ;
+}
+
+
+
+
---
+++ src/help_functions.h
@@ -0,0 +1,41 @@
+/*
+ * help_functions.h
+ *
+ *  Created on: Apr 24, 2017
+ *      Author: ramiz
+ */
+
+#ifndef HELP_FUNCTIONS_H_
+#define HELP_FUNCTIONS_H_
+
+#include <cmath>
+#include <string>
+
+class HelpFunctions {
+public:
+
+  //definition of one over square root of 2*pi:
+  const float ONE_OVER_SQRT_2PI;
+
+  //Constructor
+  HelpFunctions();
+
+  //definition square:
+  float squared(float x);
+
+  /*****************************************************************************
+   * normpdf(X,mu,sigma) computes the probability function at values x using the
+   * normal distribution with mean mu and standard deviation std. x, mue and
+   * sigma must be scalar! The parameter std must be positive.
+   * The normal pdf is y=f(x;mu,std)= 1/(sqrt(2pi * square(std)) * e ^ [ -(x−mu)^2 / 2*std^2 ]
+  *****************************************************************************/
+  float Normpdf(float x, float mu, float std);
+
+  //function to normalize a vector:
+  std::vector<float> NormalizeVector(std::vector<float> inputVector);
+};
+
+
+
+
+#endif /* HELP_FUNCTIONS_H_ */
