3a81694ae6574d2e233d08730739c4f88fabe9e9
Ramiz Raja <informramiz@gmail.com>
2017-07-26 08:48:47 +0500

    refactor: Change training and testing data path

--- src/main.cpp
+++ src/main.cpp
@@ -61,10 +61,10 @@ vector<string> Load_Label(string file_name)
 
 int main() {
 
-    vector< vector<double> > X_train = Load_State("./train_states.txt");
-    vector< vector<double> > X_test  = Load_State("./test_states.txt");
-    vector< string > Y_train  = Load_Label("./train_labels.txt");
-    vector< string > Y_test   = Load_Label("./test_labels.txt");
+    vector< vector<double> > X_train = Load_State("./data/train_states.txt");
+    vector< vector<double> > X_test  = Load_State("./data/test_states.txt");
+    vector< string > Y_train  = Load_Label("./data/train_labels.txt");
+    vector< string > Y_test   = Load_Label("./data/test_labels.txt");
 
   cout << "X_train number of elements " << X_train.size() << endl;
   cout << "X_train element size " << X_train[0].size() << endl;
